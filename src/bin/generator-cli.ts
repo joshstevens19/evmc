1a4710f5923265ae5362fce1e4138bf18a1a7450
Josh Stevens <joshstevens19@hotmail.co.uk>
2022-11-06 11:50:36 +0000

    feat: generator cli

---
+++ src/bin/generator-cli.ts
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+
+import 'reflect-metadata';
+import { execute } from './execute';
+
+// we can create typings for this but its just getting the version
+// if we import it then it wants the package.json in the src folder
+// which we dont want
+// tslint:disable-next-line: no-var-requires
+const packageJson = require('../../package.json');
+
+(async () => {
+  await execute(packageJson);
+})().catch((err) => console.error(err.message));
